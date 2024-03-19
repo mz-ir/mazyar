246b18cf89e747b6b2615d39f0432d420270de5f
Managerzone Iran <146381760+mz-ir@users.noreply.github.com>
2024-03-19 10:06:52 +0330

    bump version to 2.4

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Mazyar
 // @namespace    http://tampermonkey.net/
-// @version      2.3
+// @version      2.4
 // @description  Swiss Army knife for managerzone.com
 // @copyright    z7z from managerzone.com
 // @author       z7z from managerzone.com
