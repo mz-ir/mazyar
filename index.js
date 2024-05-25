ce8a8fe42b4c77c9c8f1540beb6b36352ea190e0
ManagerZone Iran <146381760+mz-ir@users.noreply.github.com>
2024-05-26 01:00:51 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Mazyar
 // @namespace    http://tampermonkey.net/
-// @version      2.12
+// @version      2.13
 // @description  Swiss Army knife for managerzone.com
 // @copyright    z7z from managerzone.com
 // @author       z7z from managerzone.com
